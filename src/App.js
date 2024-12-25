23b1a7c1ca0040465caa19f42ee7cb1f3cca7541
Shubhangi Gupta <shubhangi.gupta@cynoteck.com>
2024-12-25 21:19:26 +0530

    push change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router";
+import Homepage from "./page/Homepage";
+import Profilepage from "./page/Profilepage";
+import Header from "./Components/Header";
+import UserProvider from "./context/UserProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <UserProvider> */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/profile/:id" element={<Profilepage />} />
+        </Routes>
+      {/* </UserProvider> */}
+    </>
   );
 }
 
